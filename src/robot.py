9bace41205b0b6e98573c4238b9c673beb15f642
Piotr Marecki <pmarecki@gmail.com>
2023-10-07 13:23:53 +0200

    feat: add some implementation of a mug

--- src/robot.py
+++ src/robot.py
@@ -1,15 +1,19 @@
 # Klasa --- ma "pola" (fields) i "metody/funkcje"
+from random import randint
+
+
 class Robot:
 
     def __init__(self, name: str):
         print('uruchamiam konstruktor klasy A')
         self.name = name  # pole
+        self.robot_id = randint(0, 100000)  # pole, ale nie będące argumentem konstuktora
 
     def foo(self):  # metoda
         name = 'Kadabra'
         return f'my name is {self.name}'
 
-    def fill(self, content_type: str, content_amount: float):   # metoda z argumentami
+    def fill(self, content_type: str, content_amount: float):  # metoda z argumentami
         pass
 
     def add(self, a: int, b: int):
@@ -24,5 +28,4 @@ b = Robot('Li')  # tworzenie drugiej instancji
 print(a.foo())
 print(b.foo())
 
-a.fill('cola', 12.0)    # wywołanie  metody z argumentami
-
+a.fill('cola', 12.0)  # wywołanie  metody z argumentami
