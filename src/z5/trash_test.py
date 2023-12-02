e51aee1652a465287809248bc696cda835433f21
Piotr Marecki <pmarecki@gmail.com>
2023-12-02 13:20:51 +0100

    feat: small test-specification of how iteration should work

--- src/z5/trash_test.py
+++ src/z5/trash_test.py
@@ -11,3 +11,10 @@ class TrashTest(unittest.TestCase):
     def test_can_add(self):
         self.testee[3] = 10
         self.assertTrue(10 in self.testee)
+
+    def test_can_iterate(self):
+        self.testee[2] = 10
+        self.testee[4] = 20
+
+        vals = list(self.testee)
+        self.assertEqual(vals, [10, 20])
