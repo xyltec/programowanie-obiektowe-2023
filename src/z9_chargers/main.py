27dc61450dcdfff34f0acb0a5a50a27cbc2a1014
xyltec <125214164+xyltec@users.noreply.github.com>
2024-02-17 12:57:17 +0100

    Update main.py

--- src/z9_chargers/main.py
+++ src/z9_chargers/main.py
@@ -38,3 +38,61 @@ class Charger:
         self.total_charged_kw = 0
         self.attached_car_vin = None
         self.status = ChargerStatus.FREE
+
+class ChargingService:
+    def __init__(self, time_modifier):
+        self.chargers = []
+        self.time_modifier = time_modifier
+
+    def start_charging(self, client_id, vin, kwh, desired_current_kw, charger_position):
+        charger = self.chargers[charger_position]
+        if charger.status == ChargerStatus.FREE and charger.max_current_kw >= desired_current_kw:
+            charger.attached_car_vin = vin
+            charger.status = ChargerStatus.CHARGING
+            charging_time_seconds = kwh / desired_current_kw * self.time_modifier
+            return ChargingSession(csid=client_id, status=ChargingStatus.OPEN, current_kw=desired_current_kw,
+                                   total_kwh=kwh), charging_time_seconds
+        else:
+            return None, None
+
+    def stop_charging(self, client_id, vin):
+        for charger in self.chargers:
+            if charger.attached_car_vin == vin:
+                charger.status = ChargerStatus.FREE
+                charger.attached_car_vin = None
+                return True
+        return False
+
+    def attach_charger(self, charger):
+        self.chargers.append(charger)
+
+    def disable_charger(self, charger_position):
+        self.chargers[charger_position].status = ChargerStatus.ERROR
+
+    def enable_charger(self, charger_position):
+        self.chargers[charger_position].status = ChargerStatus.FREE
+
+    def remove_charger(self, charger):
+        self.chargers.remove(charger)
+
+
+if __name__ == "__main__":
+
+    charger1 = Charger(max_current_kw=50)
+    charger2 = Charger(max_current_kw=100)
+
+    charging_service = ChargingService(time_modifier=3600)  # godziny
+
+    charging_service.attach_charger(charger1)
+    charging_service.attach_charger(charger2)
+
+    # rozpoczęcie ładowania
+    session, charging_time_seconds = charging_service.start_charging(client_id="client1", vin="car1", kwh=30,
+                                                                 desired_current_kw=40, charger_position=0)
+    if session:
+        print("Sesja ładowania rozpoczęta. Szacowany czas ładowania:", charging_time_seconds, "sekund")
+
+
+    stop_charging = charging_service.stop_charging(client_id="client1", vin="car1")
+    if stop_charging:
+        print("Ładowanie zostało zatrzymane.")
